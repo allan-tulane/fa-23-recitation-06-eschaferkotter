ae7e83a1da2fb4b10330bb35e8f320f7ea0fcff3
eschaferkotter <123409959+eschaferkotter@users.noreply.github.com>
2023-11-09 02:01:15 +0000

    final

--- answers.md
+++ answers.md
@@ -10,11 +10,13 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+- W(n-1) + W(n-2) + O(1) = O(2^n)
 
 - **3)**
-
+- S(n-1) + O(1) = O(n)
 - **4)**
-
+- The amount of calls is equal to the fibonacci sequence itself considering that to calculate any fib_recursive(n) it has to make calls to fib_recursive(n-1) and fib_recursive(n-2)
 - **6)**
-
+- The maximum number of times it will be called is i times as it stores the results, and later calls will just return what is stored. Max work is O(n) and span is also O(n)
 - **8)**
+- The maximum amount of times Fi is read is i times. The work and span are still O(n), comparable to top down.
--- main.py
+++ main.py
@@ -5,23 +5,39 @@ def fib_recursive(n, counts):
     each time fib_recursive(i, counts) is called.
     """    
     counts[n] += 1
-    ###TODO
-    pass
+    if n == 0:
+      return 0
+    if n == 1:
+      return 1
+    return fib_recursive(n-1,counts) + fib_recursive(n-2,counts)
     
 
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+    if n == 0:
+      return 0
+    if n == 1:
+      return 1
 
+    if fibs[n] == -1:
+      return fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+
+    return fibs[n]
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
+    if n == 0:
+      return n
+    if n == 1:
+      return n
+
+    subsequent = [0] * (n+1)
+    subsequent[1] = 1
+
+    for i in range(2,n+1):
+      subsequent[i] = subsequent[i-1] + subsequent[i-2]
+
+    return subsequent[n]
+
+  
 
 
-
-
-def fib_bottom_up_better(n):
-    ###TODO
-    pass
